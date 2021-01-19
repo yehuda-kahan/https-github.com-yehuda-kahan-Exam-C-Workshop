8958d5d2ee458fd3294b1fa7efc5a6d27b41a618
yehuda kahan <yehudajka@gmail.com>
2021-01-19 20:46:41 +0200

    comments in the main

--- FinalExamYehudaKahan/main.c
+++ FinalExamYehudaKahan/main.c
@@ -8,11 +8,13 @@
 
 int main()
 {
+    /*Create two stacks*/
     Stack *stack1 = CreateStack();
     Stack *stack2 = CreateStack();
 
     printf_s("Stack 1 :\n");
     
+    /*Insert values*/
     StackPush(stack1, 1);
     StackPush(stack1, 2);
     StackPush(stack1, 8);
@@ -20,15 +22,18 @@ int main()
 
     StackPrint(stack1);
 
+    /*Pop value*/
     T popValue = StackPop(stack1);
     printf_s("\n%i\n\n", popValue);
 
     StackPrint(stack1);
 
+    /*Delete the first stack*/
     DeleteStack(stack1);
 
     printf_s("Stack 2 :\n");
 
+    /*Insert values*/
     StackPush(stack2, 10);
     StackPush(stack2, 20);
     StackPush(stack2, 80);
@@ -36,11 +41,13 @@ int main()
 
     StackPrint(stack2);
 
+    /*Pop value*/
     popValue = StackPop(stack2);
     printf_s("\n%i\n\n", popValue);
 
     StackPrint(stack2);
 
+    /*Delete the second stack*/
     DeleteStack(stack2);
     
     return 0;
